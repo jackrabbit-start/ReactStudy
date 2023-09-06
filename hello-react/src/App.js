b5e342f308ad0ffb970d51498f26cee7244ef80a
mikle8244 <mikle8244@gmail.com>
2023-09-06 15:56:59 +0900

    9.6 lec7

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,14 +1,27 @@
 import React, { Component } from "react";
-import ScrollBox from "./ScrollBox";
+import LifeCycleSample from "./LifeCycleSample";
+import ErrorBoundary from "./ErrorBoundary";
+
+function getRandomColor() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
 
 class App extends Component {
+  state = {
+    color: "#000000",
+  };
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor(),
+    });
+  };
   render() {
     return (
       <div>
-        <ScrollBox ref={(ref) => (this.ScrollBox = ref)} />
-        <button onClick={() => this.ScrollBox.scrollToBotton()}>
-          맨 밑으로
-        </button>
+        <button onClick={this.handleClick}>랜덤색상</button>
+        <ErrorBoundary>
+          <LifeCycleSample color={this.state.color} />
+        </ErrorBoundary>
       </div>
     );
   }
